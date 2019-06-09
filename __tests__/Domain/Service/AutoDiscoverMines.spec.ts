26241cf5d377bb04b3a9f29a999301ea513f69bc
Florian Chaudret <florian.chaudret@gmail.com>
2019-06-09 10:44:39 +0200

    Fix autodiscover cells
    the autodiscover is stopped as soon as a mine is found

--- __tests__/Domain/Service/AutoDiscoverMines.spec.ts
+++ __tests__/Domain/Service/AutoDiscoverMines.spec.ts
@@ -6,7 +6,13 @@ import {AutoDiscoverMines} from "../../../src/Domain/Service/AutoDiscoverMines";
 
 describe('Auto discover mines', () => {
     it('success', () => {
-        // instance a grid.
+        /**
+         * instance a grid.
+         *
+         * [ ] [ ] [X]
+         * [ ] [ ] [ ]
+         * [ ] [ ] [ ]
+         */
         let cells: CellInterface[] = [];
         cells.push(new CellModel(0, 0, false));
         cells.push(new CellModel(1, 0, false));
@@ -24,19 +30,43 @@ describe('Auto discover mines', () => {
             expect(cell.isDiscover()).toBe(false);
         })
 
-        // service.
+        /**
+         * Expect the discover grid.
+         *
+         * [0] [1] [X]
+         * [0] [1] [1]
+         * [0] [0] [0]
+         */
         let adjacentMines = new AdjacentMines();
         let autoDiscoverMines = new AutoDiscoverMines(adjacentMines);
         autoDiscoverMines.autoDiscover(0, 0, grid);
+        // x:0 y:0
         expect(grid.getCells()[0].isDiscover()).toBe(true);
+        expect(grid.getCells()[0].getNumberOfMinesAdjacent()).toBe(0);
+        // x:1 y:0
         expect(grid.getCells()[1].isDiscover()).toBe(true);
+        expect(grid.getCells()[1].getNumberOfMinesAdjacent()).toBe(0);
+        // x:2 y:0
         expect(grid.getCells()[2].isDiscover()).toBe(true);
+        expect(grid.getCells()[2].getNumberOfMinesAdjacent()).toBe(0);
+        // x:0 y:1
         expect(grid.getCells()[3].isDiscover()).toBe(true);
-        expect(grid.getCells()[4].isDiscover()).toBe(false);
-        expect(grid.getCells()[5].isDiscover()).toBe(false);
+        expect(grid.getCells()[3].getNumberOfMinesAdjacent()).toBe(0);
+        // x:1 y:1
+        expect(grid.getCells()[4].isDiscover()).toBe(true);
+        expect(grid.getCells()[4].getNumberOfMinesAdjacent()).toBe(1);
+        // x:2 y:1
+        expect(grid.getCells()[5].isDiscover()).toBe(true);
+        expect(grid.getCells()[5].getNumberOfMinesAdjacent()).toBe(1);
+        // x:0 y:2
         expect(grid.getCells()[6].isDiscover()).toBe(true);
-        expect(grid.getCells()[7].isDiscover()).toBe(false);
+        expect(grid.getCells()[6].getNumberOfMinesAdjacent()).toBe(0);
+        // x:1 y2
+        expect(grid.getCells()[7].isDiscover()).toBe(true);
+        expect(grid.getCells()[7].getNumberOfMinesAdjacent()).toBe(1);
+        // x:2 y:2
         expect(grid.getCells()[8].isDiscover()).toBe(false);
+        expect(grid.getCells()[8].getNumberOfMinesAdjacent()).toBe(null);
 
     })
 })
--- src/Domain/Service/AutoDiscoverMines.ts
+++ src/Domain/Service/AutoDiscoverMines.ts
@@ -13,10 +13,10 @@ export class AutoDiscoverMines implements AutoDiscoverMinesInterface {
     public autoDiscover(x: number, y: number, grid: GridInterface): void {
         let cellsDiscover: CellInterface[] = this._adjacentMines.getCellsAdjacent(x, y, grid);
         cellsDiscover.forEach((cell: CellInterface) => {
-            let count = this._adjacentMines.countMines(cell.getPositionX(), cell.getPositionY(), grid);
-            if (count === 0 && cell.isDiscover() === false) {
+            if (cell.isDiscover() === false && cell.isMined() === false) {
+                let count = this._adjacentMines.countMines(cell.getPositionX(), cell.getPositionY(), grid);
+                cell.setNumberOfMinesAdjacent(count);
                 cell.setDiscover(true);
-                cell.setNumberOfMinesAdjacent(0);
                 this.autoDiscover(cell.getPositionX(), cell.getPositionY(), grid);
             }
         });
